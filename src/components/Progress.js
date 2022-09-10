bb43d4147c74872d31f7e3fc53afe83cf04aab2e
0bscurity <vestorlockwood@gmail.com>
2022-09-09 20:02:40 -0400

    Updated progress bar format

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,9 +1,10 @@
 import {ProgressBar} from "react-bootstrap";
 import React from "react";
+import './AboutMe.css';
 
 function Progress(props) {
     const progress = props.progress;
-    return <div className="my-4"><p>{props.skill}</p><ProgressBar striped variant="success" animated now={progress}/></div>
+    return <div className="me-4 my-5"><p className="progress-label">{props.skill}</p><ProgressBar variant="success" animated now={progress}/></div>
 }
 
 export default Progress;
